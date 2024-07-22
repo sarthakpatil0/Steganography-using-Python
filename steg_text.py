ddef6fad7a6d8b54faccb1062e5fa5a66777345d
Sarthak <sarthak11patil@gmail.com>
2024-07-23 00:30:56 +0530

    Update steg_text.py

--- steg_text.py
+++ steg_text.py
@@ -1,7 +1,9 @@
+#### To Add Hide Text in the Image ####
 #with open('imag.jpeg', 'ab') as f:
 #    f.write(b"Hello World")
 
 
+#### To Extract the Text from the Image ####
 with open('imag.jpeg', 'rb') as f:
     text = f.read()
     offset = text.index(bytes.fromhex('FFD9'))
